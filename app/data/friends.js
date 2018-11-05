243b037fa7a0a95d50c7d22026b0c2d9987a0e84
TomSmaj <thomas.smajstrla@gmail.com>
2018-11-04 19:31:34 -0600

    added modal popup

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 var friendData = [
     {
-        "name":"Frank",
+        "name":"Tom Waits",
         "photo":"http://www.tomwaitslibrary.info/images/imagesfilmography/92-goingoutwest.jpg",
         "scores":[1, 4, 3, 5, 2, 2, 3, 4, 5, 4]
     }
